99559c321a5172498987d29a5268fc0237766a54
feureau <feureau@gmail.com>
2014-04-04 06:34:21 +0700

    Fix Rotation

--- Space Shooter/Assets/Scripts/PlayerControl.cs
+++ Space Shooter/Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,10 @@ public class Boundary
 	public float xMax;
 	public float zMin;
 	public float zMax;
+	public float rotateXMax,rotateZMax;
+	public float rotateXMin;
+	public float rotateZMin;
+
 }
 
 public class PlayerControl : MonoBehaviour {
@@ -20,15 +24,14 @@ public class PlayerControl : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement1 = new Vector3(moveHorizontal,0.0f,moveVertical);
 		rigidbody.velocity = movement1*speed;
-		rigidbody.position = new Vector3(Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),0.0f,Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
-
-
-
-		if (Input.anyKey==false) {
-			rigidbody.AddRelativeTorque(rigidbody.rotation.x*-10, 0.0f, rigidbody.rotation.z*-10, ForceMode.VelocityChange); // if no button pressed, return to default rotation, eliminating torque
-		}else{
-			rigidbody.AddRelativeTorque(rigidbody.velocity.z*pitchMultiplier, 0.0f, rigidbody.velocity.x*-rollMultiplier,ForceMode.VelocityChange); //rotate roll and pitch in relation of movement
-		}
-		rigidbody.rotation = Quaternion.Euler( new Vector3(Mathf.Clamp(rigidbody.rotation.x, 0, 45),0.0f,Mathf.Clamp(rigidbody.rotation.z, 0, 45)));
+		rigidbody.position = new Vector3(
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+			);
+		rigidbody.rotation = Quaternion.Euler (
+			rigidbody.velocity.z*pitchMultiplier, 
+			0.0f, 
+			rigidbody.velocity.x*rollMultiplier*-1);
 	}
 }
